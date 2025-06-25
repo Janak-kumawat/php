10e1df764677814445e992355175abe8f7b4dc8e
Janak-kumawat <kumawatjanak0806@gmail.com>
2025-06-25 12:19:51 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./server/style.css">
+</head>
+<body>
+    
+</body>
+</html>
+<div class = "container">
+    <h1 class="heading">sign up</h1>
+    <form>
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="username" class="form-label" >Username</label>
+    <input type="text" class="form-control" id="username" placeholder="Enter your name">
+  </div>
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="useremail" class="form-label" >Useremail</label>
+    <input type="email" class="form-control" id="useremail" placeholder="Enter your email">
+  </div>
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="address" class="form-label" >address</label>
+    <input type="address" class="form-control" id="address" placeholder="Enter your address">
+  </div>
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="password" class="form-label" >Password</label>
+    <input type="password" class="form-control" id="password" placeholder="Enter your password">
+  </div>
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+  <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+  
+</form>
+
+</div>
